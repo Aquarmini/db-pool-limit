2dcd2694a466f3688757c91da1586e52040358ed
李铭昕 <715557344@qq.com>
2020-09-19 13:35:51 +0800

    Update

--- app/Service/UserLevelServiceInterface.php
+++ app/Service/UserLevelServiceInterface.php
@@ -13,4 +13,29 @@ namespace App\Service;
 
 interface UserLevelServiceInterface
 {
+    /**
+     * 获取当前用户等级.
+     */
+    public function getLevel(int $userId): int;
+
+    /**
+     * 获取当前用户下级所需金额.
+     */
+    public function getRequiredAmount(int $userId): int;
+
+    /**
+     * 获取用户当前等级的信息.
+     */
+    public function getLevelInfo(int $userId): array;
+
+    /**
+     * 当前进货额.
+     */
+    public function getAmount(int $userId): float;
+
+    /**
+     * 方法1
+     * 增加进货额.
+     */
+    public function addAmount(int $userId, float $amount): int;
 }
--- app/Service/UserOrderServiceInterface.php
+++ app/Service/UserOrderServiceInterface.php
@@ -11,6 +11,19 @@ declare(strict_types=1);
  */
 namespace App\Service;
 
+use App\Model\Order;
+
 interface UserOrderServiceInterface
 {
+    /**
+     * 方法1
+     * 订单支付后，根据订单增加用户进货额
+     */
+    public function handle(Order $order): bool;
+
+    /**
+     * 方法1
+     * 订单退款后，根据订单减少用户进货额
+     */
+    public function refund(Order $order): bool;
 }
